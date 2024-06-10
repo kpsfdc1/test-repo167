7611e423d15ac405031cbde4e22cfda4eb5b9e65
dev1 <dev1@mail.com>
2024-06-10 08:16:16 +0530

    modf by dev1

--- hello.java
+++ hello.java
@@ -1,7 +1,7 @@
-class hello
+class hello	#from dev1
 {
-	pubic static void main()
+	pubic static void main()	#from dev1
 	{
-		system.out.println("hello modf");
+		system.out.println("hello modf by dev1");
 	}
 }
